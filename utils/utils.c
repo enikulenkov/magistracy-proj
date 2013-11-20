671bbe7ea1774dfb5624f8e2079b484f1c8be4b1
Egor Nikulenkov <enikulenkov@gmail.com>
2013-11-20 21:21:54 +0400

    Deleted unused code

--- utils/utils.c
+++ utils/utils.c
@@ -4,19 +4,6 @@
 #include <string.h>
 #include "utils_internal.h"
 
-/*#define MAX_PREAMBLE_FLD_NAME_LEN    sizeof("columns")*/
-/*#define MAX_PREAMBLE_FLD_SSCANF_STR  4*/
-
-/*typedef struct preamble_field_s*/
-/*{*/
-  /*char name[MAX_PREAMBLE_FLD_NAME_LEN];*/
-  /*int  name_len;*/
-  /*char sscanf_str[MAX_PREAMBLE_FLD_SSCANF_STR];*/
-  /*int  sscanf_str_len;*/
-/*}*/
-/*preamble_field_t;*/
-
-
 int check_vectors_for_eq(double *v1, double *v2, int n)
 {
   int i;
