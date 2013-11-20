6135c84b850a4035ba2d900863460ca78db6ef42
Egor Nikulenkov <enikulenkov@gmail.com>
2013-11-21 00:35:39 +0400

    Add random_matr()

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H 1
 
+#define IDX(_r, _c, _n) ((_r)*(_n) + (_c))
+
 int check_vectors_for_eq(double *v1, double *v2, int n);
+void random_matr(double *A, int n, int m);
 
 #endif /* UTILS_H */
--- utils/utils.c
+++ utils/utils.c
@@ -3,6 +3,21 @@
 #include <math.h>
 #include <string.h>
 #include "utils_internal.h"
+#include "utils.h"
+
+void random_matr(double *A, int n, int m)
+{
+  int i,j;
+
+  for (i=0; i<n; i++)
+  {
+    for (j=0; j < m; j++)
+    {
+      A[ IDX(i,j,m) ] = (double)rand() / (double)RAND_MAX;
+    }
+  }
+}
+
 
 int check_vectors_for_eq(double *v1, double *v2, int n)
 {
