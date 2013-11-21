2d2ff86e2aa809f79cc36c9615aa02bf972294d6
Egor Nikulenkov <enikulenkov@gmail.com>
2013-11-22 01:25:46 +0400

    Bunch of fixes for cublas routine in matrMult performance test

--- tests/matr_mult_perf/main.c
+++ tests/matr_mult_perf/main.c
@@ -6,7 +6,7 @@
 #include "timer.h"
 #include "utils.h"
 
-int g_sizes[] = {1000, 2000, 3500, 5000};
+int g_sizes[] = {1000, 2000, 3500, 5000, 6500};
 
 #define SIZES_COUNT sizeof(g_sizes)/sizeof(g_sizes[0])
 
@@ -22,57 +22,80 @@ void calc_blas(double *A, double *B, double *C, int n)
 
 void calc_cublas(double *A, double *B, double *C, int n)
 {
-  cudaError_t cudaStat;
   cublasStatus_t stat;
+#define CUDA_CALL(_call) \
+  { \
+    cudaError_t cudaStat = (_call); \
+    if ( cudaStat != cudaSuccess) { \
+      printf("CUDA Error: %s (error %d)\n", cudaGetErrorString(cudaStat), cudaStat); \
+      goto error; \
+    } \
+  }
+
+#define CUBLAS_CALL(_call, _msg) \
+  { \
+    cublasStatus_t stat = (_call); \
+    if ( stat != CUBLAS_STATUS_SUCCESS) { \
+      printf("CUBLAS Error:" _msg ", (error %d)\n", stat); \
+      goto error; \
+    } \
+  }
+ 
   cublasHandle_t handle;
+  cudaEvent_t copyStart, copyEnd;
+  float elapsedTime;
 
   double *dA;
   double *dB;
   double *dC;
 
-  cudaStat = cudaMalloc((void **)dA, n*n*sizeof(*dA));
-  cudaStat = cudaMalloc((void **)dB, n*n*sizeof(*dA));
-  cudaStat = cudaMalloc((void **)dC, n*n*sizeof(*dA));
+  CUDA_CALL( cudaMalloc((void **)&dA, n*n*sizeof(double)));
+  CUDA_CALL( cudaMalloc((void **)&dB, n*n*sizeof(double)));
+  CUDA_CALL( cudaMalloc((void **)&dC, n*n*sizeof(double)));
 
-  stat = cublasCreate(&handle);
+  CUBLAS_CALL( cublasCreate(&handle), "Cublas init error");
 
-  if (stat != CUBLAS_STATUS_SUCCESS)
-  {
-    printf("Cublas init error!!!\n");
-    goto error;
-  }
+  cudaEventCreate(&copyStart);
+  cudaEventCreate(&copyEnd);
 
-  stat = cublasSetMatrix(n, n, sizeof(double), A, n, dA, n);
-  if (stat != CUBLAS_STATUS_SUCCESS)
-  {
-    printf("Cublas setMatrix error!!!\n");
-    goto error;
-  }
+  cudaEventRecord(copyStart, 0);
 
-  stat = cublasSetMatrix(n, n, sizeof(double), B, n, dB, n);
-  if (stat != CUBLAS_STATUS_SUCCESS)
-  {
-    printf("Cublas setMatrix error!!!\n");
-    goto error;
-  }
+  CUBLAS_CALL( cublasSetMatrix(n, n, sizeof(double), A, n, dA, n), "Cublas setMatrix A error");
+
+  CUBLAS_CALL( cublasSetMatrix(n, n, sizeof(double), B, n, dB, n), "Cublas setMatrix B error");
+
+  cudaEventRecord(copyEnd, 0);
+  cudaEventSynchronize(copyEnd);
+
+  cudaEventElapsedTime(&elapsedTime, copyStart, copyEnd);
+
+  printf("cuBlas copy to device: %3.1f ms\n", elapsedTime);
 
   {
     cublasOperation_t trans = CUBLAS_OP_N;
     double alpha = 1.0;
     double beta = 0.0;
+    cudaError_t err = 0;
 
-    cublasDgemm(handle, trans, trans, n, n, n, &alpha, A, n, B, n, &beta, C, n);
+    CUBLAS_CALL( cublasDgemm(handle, trans, trans, n, n, n, &alpha, dA, n, dB, n, &beta, dC, n), "DGEMM error");
 
-    stat = cublasGetMatrix(n, n, sizeof(double), dC, n, C, n);
+    err = cudaDeviceSynchronize();
 
-    if (stat != CUBLAS_STATUS_SUCCESS)
-    {
-      printf("Cublas data upload error!!!\n");
-      goto error;
-    }
+    cudaEventRecord(copyStart, 0);
+
+    CUBLAS_CALL( cublasGetMatrix(n, n, sizeof(double), dC, n, C, n), "Cublas getMatrix C error");
+
+    cudaEventRecord(copyEnd, 0);
+    cudaEventSynchronize(copyEnd);
+
+    cudaEventElapsedTime(&elapsedTime, copyStart, copyEnd);
+
+    printf("cuBlas copy from device: %3.1f ms\n", elapsedTime);
   }
 
 error:
+  cudaEventDestroy(copyStart);
+  cudaEventDestroy(copyEnd);
   cudaFree(dA);
   cudaFree(dB);
   cudaFree(dC);
@@ -99,7 +122,7 @@ void run_test(int n)
   printf("Blas time: %s\n", timer_diff_as_str(&timer));
 
   timer_start(&timer);
-  calc_blas(A, B, C, n);
+  calc_cublas(A, B, C, n);
   timer_stop(&timer);
   printf("cuBlas time: %s\n", timer_diff_as_str(&timer));
 
