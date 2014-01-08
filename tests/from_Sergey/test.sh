54e64d413a63c967b4fbe2f990257ebd61fa9020
Egor Nikulenkov <enikulenkov@gmail.com>
2014-01-09 01:47:20 +0400

    Updated test from Sergey

--- tests/from_Sergey/test.sh
+++ tests/from_Sergey/test.sh
@@ -1,11 +1,15 @@
 SIZES="10 100 1000 2000"
+OUTDIR=results
+
+rm -rf $OUTDIR
+
+mkdir -p $OUTDIR
 
 for i in $SIZES ; do 
   echo "Size: $i"
-  echo "--CBLAS--"
-  C_BLAS/cblas_main $i
-  echo "--CUBLAS--"
-  CUBLAS/cublas_main $i
-  echo ""
+  echo "CBLAS..."
+  C_BLAS/cblas_main $i > $OUTDIR/cblas_$i.txt
+  echo "CUBLAS..."
+  CUBLAS/cublas_main $i > $OUTDIR/cublas_$i.txt
   echo "----------"
 done
