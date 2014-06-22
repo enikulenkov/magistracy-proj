1db28964383053e12c6ef8ebb550cb5df613a34d
Egor Nikulenkov <enikulenkov@gmail.com>
2014-06-22 18:47:32 +0400

    Added visualization

---
+++ TGROUP/Ir_N/visualize_cluster.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ga_utils.h"
+
+int main(int argc, char *argv[])
+{
+  int total_atoms_cnt;
+  double *coords;
+  char *in_file;
+  char *out_file;
+
+  if (argc < 3)
+  {
+    fprintf(stderr, "Usage: %s {input_file.mat} {output_file.cml}", argv[0]);
+    return 1;
+  }
+
+  in_file = argv[1];
+  out_file = argv[2];
+
+  total_atoms_cnt = read_input(in_file, &coords);
+
+  if (total_atoms_cnt < 0)
+  {
+    DBG_LOG("Invalid input, exit...");
+    return 1;
+  }
+
+  DBG_LOG("Loaded %d atoms' coordinates\n", total_atoms_cnt);
+
+  write_output(out_file, coords, total_atoms_cnt);
+
+  printf("Result is written to %s", out_file);
+
+  free(coords);
+
+  return 0;
+}
