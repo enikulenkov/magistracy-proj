15cf471d856b55579006c3379ac624fe8000ac23
Egor Nikulenkov <enikulenkov@gmail.com>
2014-06-23 16:07:38 +0400

    improved run.sh

--- TGROUP/Ir_N/run.sh
+++ TGROUP/Ir_N/run.sh
@@ -7,15 +7,23 @@ MMD_RES_FILE=${DEST_DIR}/mmd_res.cml
 C_RES_FILE=${DEST_DIR}/c_res.mat
 VIS_FILE=${DEST_DIR}/c_vis.cml
 ORIG_VIS_FILE=${DEST_DIR}/orig.cml
+GA_INI_FILE=./gen_alg_prefs.ini
 TIME=/usr/bin/time
+RUN_MMD=1
 
 mkdir -p ${DEST_DIR}
 cp $1 ${DEST_DIR}
-GA_OUTPUT=`$TIME -f "time %E" ./ga $1 ${C_RES_FILE}`
-MMD_OUTPUT=`$TIME -f "time %E" ./sc_qdm_n $1`
-cp out_f.cml ${DEST_DIR}/
-./visualize_cluster ${C_RES_FILE} ${VIS_FILE}
-./visualize_cluster $1 ${ORIG_VIS_FILE}
+echo "Running GA..."
+GA_OUTPUT=`$TIME -f "time %E" 2>&1 ./ga $1 ${C_RES_FILE}`
+cp ${GA_INI_FILE} ${DEST_DIR}/
+if [ $RUN_MMD -gt 0 ] ; then
+  echo "Running MMD..."
+  MMD_OUTPUT=`$TIME -f "time %E" 2>&1 ./sc_qdm_n $1`
+  cp out_f.cml ${DEST_DIR}/
+fi
+echo "Visualizing results"
+./visualize_cluster ${C_RES_FILE} ${VIS_FILE} > /dev/null
+./visualize_cluster $1 ${ORIG_VIS_FILE} > /dev/null
 
 touch ${DEST_DIR}/summary.txt
 
@@ -23,6 +31,8 @@ echo "GA output:" >> ${DEST_DIR}/summary.txt
 echo "" >> ${DEST_DIR}/summary.txt
 echo "$GA_OUTPUT" >> ${DEST_DIR}/summary.txt
 echo "" >> ${DEST_DIR}/summary.txt
-echo "MMD output:" >> ${DEST_DIR}/summary.txt
-echo "" >> ${DEST_DIR}/summary.txt
-echo "$MMD_OUTPUT" >> ${DEST_DIR}/summary.txt
+if [ $RUN_MMD -gt 0 ] ; then
+  echo "MMD output:" >> ${DEST_DIR}/summary.txt
+  echo "" >> ${DEST_DIR}/summary.txt
+  echo "$MMD_OUTPUT" >> ${DEST_DIR}/summary.txt
+fi
